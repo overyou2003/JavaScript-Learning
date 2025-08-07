ce4a4a34c72535d4ba329fa3852ae305bc532506
overyou2003 <s6506021620032@email.kmutnb.ac.th>
2025-08-07 15:32:05 +0700

    commit

--- script/app.js
+++ script/app.js
@@ -1,1 +1,6 @@
-console.log("Hello")
+const name = prompt('What is your name?')
+const acceptTerm = confirm('Do you accept the terms?')
+
+const result = `Hello ${name} , you accepted the terms? ${acceptTerm}`
+alert(result)
+console.log(result)
