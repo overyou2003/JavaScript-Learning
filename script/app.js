f7741db93364083629efc7d354d1ba7408a7e2bc
Patiphat <youaremyhopes@gmail.com>
2025-08-10 02:25:19 +0700

    commit 9 august

--- script/app.js
+++ script/app.js
@@ -16,3 +16,11 @@ console.log(`${firstName}   ${lastName} ${age}`)
 function Name() {
     console.log('HEELO THIS IS FUNTION')
 }
+
+function closeModal() {
+    $('.modal').css('display' , 'none');
+}
+
+function openSignIn() {
+    $("#modalSignin").css('display' , 'flex');
+}
