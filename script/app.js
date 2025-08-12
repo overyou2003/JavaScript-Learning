c5d160775aa4e811bfd5212cfe345cbe83b10479
overyou2003 <s6506021620032@email.kmutnb.ac.th>
2025-08-12 23:50:12 +0700

    commit

--- script/app.js
+++ script/app.js
@@ -1,9 +1,4 @@
-// const name = prompt('What is your name?')
-// const acceptTerm = confirm('Do you accept the terms?')
-
-// const result = `Hello ${name} , you accepted the terms? ${acceptTerm}`
-// alert(result)
-// console.log(result)
+z
 
 
 const firstName = 'Patiphat'
