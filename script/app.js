09c83cbe11ff7ff9f9c8fe108920662fe2d537d3
Patiphat <youaremyhopes@gmail.com>
2025-08-25 23:06:16 +0700

    first commit

---
+++ script/app.js
@@ -0,0 +1,30 @@
+
+
+
+const firstName = 'PATIPHATTT'
+const lastName = 'Boonanan'
+const age = 18
+
+console.log(`${firstName}   ${lastName} ${age}`)
+
+
+function Name() {
+    console.log('HI HI HELLO HIHI')
+    console.log('Enimie nimo')
+}
+
+function closeModal() {
+    $('.modal').css('display' , 'none');
+}
+
+function openSignIn() {
+    $("#modalSignin").css('display' , 'flex');
+}
+
+console.log("Dreams Hello Dreams")
+
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
