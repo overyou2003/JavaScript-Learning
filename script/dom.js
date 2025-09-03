c2712121efc0d4c71c0f38f7b36b780674318999
overyou2003 <s6506021620032@email.kmutnb.ac.th>
2025-09-03 20:40:02 +0700

    commit

--- script/dom.js
+++ script/dom.js
@@ -1,3 +1,44 @@
 document.addEventListener('DOMContentLoaded' , () => {
-    console.log('test')
+    const login = document.getElementById('login')
+    const userArea = document.getElementById('userArea')
+
+    login.addEventListener('submit' , async (e) => {
+        e.preventDefault()
+        console.log('test')
+        const username = document.getElementById('username').value
+        const password = document.getElementById('password').value
+        const btnLogin = document.getElementById('btnLogin')
+        btnLogin.textContent = 'Loading...'
+        btnLogin.disabled = true
+        
+        try {
+            const response = await fetch('https://dummyjson.com/auth/login' , {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({username,password})
+            })
+            if (response.ok) {
+                const data = await response.json()
+                alert(JSON.stringify(data))
+                const token = data.accessToken
+                login.style.display = 'none'
+                userArea.style.display = 'block'
+                const btnGetCurrentUser = document.getElementById('btnGetCurrentUser')
+                btnGetCurrentUser.addEventListener('click' , async (e) => {
+                    try {
+                        const response = await fetch('https://dummyjson.com/users/me' , {
+                            headers: { Authorization: `Bearer ${token}` } ,
+                        })
+                        if (response.ok) {
+                            const data = await response.json();
+                        }
+                    } catch {
+
+                    }
+                })
+            }
+        } catch {
+            
+        }
+    })
 })
